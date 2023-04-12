1853ad37869f3bab93c587d35756d23241b9f0d0
Remi-dee <oluwaremidaniel36@gmail.com>
2023-04-12 08:24:48 -0700

    Update portfolio day 3

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "./header.css";
 import CTA from "./CTA";
-{/*import ME from "../../Asset/menew.png";*/}
-import ME from "../../Asset/me.jpg";
+import ME from "@Asset/self.jpg";
+
 import HeaderSocials from "./HeaderSocials";
 
 const header = () => {
@@ -12,17 +12,16 @@ const header = () => {
         <div className="me__avatar">
           <img src={ME} alt="Avatar One" />
         </div>
-        
+
         <h5>Hello I'm </h5>
         <h1>Remi Daniel</h1>
         <h5 className="text-light">Frontend Developer</h5>
-        <CTA />
         <HeaderSocials />
-        <div className="me">
-          {/*<img src={ME} alt="me" />*/}
-        </div>
-        <a href="#portfolio" className="scroll__down">
-          Portfolio
+        <CTA />
+
+        <div className="me">{/*<img src={ME} alt="me" />*/}</div>
+        <a href="#contact" className="scroll__down">
+          Contact Me
         </a>
       </div>
     </header>
