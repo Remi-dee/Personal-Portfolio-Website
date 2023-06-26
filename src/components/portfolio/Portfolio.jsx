474b1a18fdabf0a34a3df23a695b2e383b6770a4
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2023-06-26 11:29:52 +0100

    Updated

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,104 +1,110 @@
 import React from "react";
 import "./portfolio.css";
-import IMG1 from "../../Asset/portfolio1.jpg";
-import IMG2 from "../../Asset/portfolio2.jpg";
-import IMG3 from "../../Asset/portfolio3.jpg";
-import IMG4 from "../../Asset/portfolio4.jpg";
-import IMG5 from "../../Asset/portfolio5.jpg";
-import IMG6 from "../../Asset/portfolio6.jpg";
 
-const data = [
-  {
-    id: 1,
-    category: "Solo Project",
-    image: IMG2,
-    title: "Figma dashboard UI kit for data design web apps",
-    github: "https://github.com",
-    demo: "https://dribbble.com/shots/20179884-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
-  },
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { data } from "./data";
 
-  /*{
-    id: 2,
-    category: "Solo Project",
-    image: "",
-    title: "Figma dashboard UI kit for data design web apps",
-    github: "https://github.com",
-    demo: "https://dribbble.com/shots/20179855-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
-  },*/
 
-  {
-    id: 2,
-    category: "Team Project",
-    image: IMG4,
-    title: "Orion UI kit for Figma",
-    github: "https://github.com",
-    demo: "https://dribbble.com/shots/20111348-Orion-UI-kit-for-Figma",
-  },
 
-  /*{
-    id: 4,
-    category: "Team Project",
-    image: IMG5,
-    title: "Figma dashboard UI kit for data design web apps",
-    github: "https://github.com",
-    demo: "https://dribbble.com/shots/20111287-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps",
-  },*/
-
-  /* {
-    id: 5,
-    category: "Archive Project",
-    image: IMG6,
-    title: "Orion UI kit for Figma",
-    github: "https://github.com",
-    demo: "https://dribbble.com/shots/20111276-Orion-UI-kit-for-Figma",
-  },*/
-
-  {
-    id: 3,
-    category: "Archive Project",
-    image: "https://res.cloudinary.com/dvwiiyev8/image/upload/v1681572853/Portfolio1.jpg",
-    title: "Random Quote Generator",
-    github: "https://github.com/Remi-dee/RandomQuoteGenerator/blob/main/README.md",
-    demo:"https://remi-dee.github.io/RandomQuoteGenerator/",
-  },
-];
 
 const portfolio = () => {
   return (
     <section id="portfolio">
       <h5>My Recent Works</h5>
       <h2>Portfolio</h2>
+
       <div className="container portfolio__container">
-        {data.map(({ id, category, image, title, github, demo }) => {
-          return (
-            <article key={id} className="portfolio__item">
-              <p className="portfolio__category">{category}</p>
-              <div className="portfolio__item-image">
-                <img
-                  src={image}
-                  alt="Image"
-                />
-              </div>
-              <h3>{title}</h3>
-              <div className="portfolio__item-cta">
-                <a
-                  href={github}
-                  className="btn btn-dark"
-                  target="_blank"
-                >
-                  Github Link
-                </a>
-                <a
-                  href={demo}
-                  className="btn btn-primary"
-                  target="_blank"
-                >
-                  Live Demo
-                </a>
-              </div>
-            </article>
-          );
-        })}
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showIndicators={true}
+          showThumbs={false}
+          interval={5000}
+        >
+          {data.slice(2, 5).map(({ id, category, image, title, github, demo }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <p className="portfolio__category">{category}</p>
+                <div className="portfolio__item-image">
+                  <img src={image} alt="Image" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn btn-dark" target="_blank">
+                    Github Link
+                  </a>
+                  <a href={demo} className="btn btn-primary" target="_blank">
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            );
+          })}
+        </Carousel>
+      </div>
+
+      <div className="container portfolio__container">
+        <Carousel
+          autoPlay
+          infiniteLoop
+          showStatus={false}
+          showIndicators={true}
+          showThumbs={false}
+          interval={5000}
+        >
+          {data.slice(0, 2).map(({ id, category, image, title, github, demo }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <p className="portfolio__category">{category}</p>
+                <div className="portfolio__item-image">
+                  <img src={image} alt="Image" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn btn-dark" target="_blank">
+                    Github Link
+                  </a>
+                  <a href={demo} className="btn btn-primary" target="_blank">
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            );
+          })}
+        </Carousel>
+      </div>
+
+      <div className="container portfolio__container">
+        <Carousel
+           autoPlay
+           infiniteLoop
+           showStatus={false}
+           showIndicators={true}
+           showThumbs={false}
+           interval={5000}
+        >
+          {data.slice(5, data.length).map(({ id, category, image, title, github, demo }) => {
+            return (
+              <article key={id} className="portfolio__item">
+                <p className="portfolio__category">{category}</p>
+                <div className="portfolio__item-image">
+                  <img src={image} alt="Image" />
+                </div>
+                <h3>{title}</h3>
+                <div className="portfolio__item-cta">
+                  <a href={github} className="btn btn-dark" target="_blank">
+                    Github Link
+                  </a>
+                  <a href={demo} className="btn btn-primary" target="_blank">
+                    Live Demo
+                  </a>
+                </div>
+              </article>
+            );
+          })}
+        </Carousel>
       </div>
     </section>
   );
---
+++ src/components/portfolio/data.js
@@ -0,0 +1,69 @@
+import IMG1 from "../../Asset/portfolio1.jpg";
+import IMG2 from "../../Asset/portfolio2.jpg";
+import IMG3 from "../../Asset/portfolio3.jpg";
+import IMG4 from "../../Asset/portfolio4.jpg";
+import IMG5 from "../../Asset/portfolio5.jpg";
+import IMG6 from "../../Asset/portfolio6.jpg";
+
+const data = [
+  {
+    id: 0,
+    category: "Solo Project",
+    image: IMG1,
+    title: "Shopzone an ecommerces app that behaves like Amazone",
+    github: "https://github.com/Remi-dee/Shop-zone",
+    demo: "https://shop-zone-six.vercel.app/",
+  },
+
+  {
+    id: 1,
+    category: "Solo Project",
+    image: IMG2,
+    title: "Trello-lite: A task setting app with AI integration",
+    github: "https://github.com/Remi-dee/Shop-zone",
+    demo: "https://shop-zone-six.vercel.app/",
+  },
+
+  {
+    id: 2,
+    category: "Team Project",
+    image: IMG3,
+    title:
+      "P2PCoder App: A platform where users can code an collaborate in real time",
+    github: "https://github.com/annonymousauthority/P2PCoder",
+    demo: "https://dribbble.com/shots/20111348-Orion-UI-kit-for-Figma",
+  },
+
+  {
+    id: 3,
+    category: "Team Project",
+    image: IMG4,
+    title:
+      "ThoughtFlow is a privacy-friendly web app that allow for journaling thoughts/perspectives about anything in an easy to use interface.",
+    github: "https://github.com/chingu-voyages/v44-tier3-team-42/tree/dev",
+    demo: "https://thoughtflow.vercel.app/",
+  },
+
+  {
+    id: 4,
+    category: "Team Project",
+    image: IMG5,
+    title:
+      "An E-book library to explore interesting and resourceful tech related contents",
+    github: "https://github.com/adetoye-dev/books-library-app",
+    demo: "https://dribbble.com/shots/20111348-Orion-UI-kit-for-Figma",
+  },
+
+  {
+    id: 5,
+    category: "Archive Project",
+    image:
+      "https://res.cloudinary.com/dvwiiyev8/image/upload/v1681572853/Portfolio1.jpg",
+    title: "Random Quote Generator",
+    github:
+      "https://github.com/Remi-dee/RandomQuoteGenerator/blob/main/README.md",
+    demo: "https://remi-dee.github.io/RandomQuoteGenerator/",
+  },
+];
+
+export { data };
