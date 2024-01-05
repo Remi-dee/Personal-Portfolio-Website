29b8b41c4145e894f059fd0e1549b9d33eec056e
Remi Daniel Aluko~ <oluwaremidaniel36@gmail.com>
2024-01-05 12:07:52 +0000

    Friday, January 5, 2024 at 1:07 PM West Africa Standard Time

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,14 @@ import Portfolio from "./components/portfolio/Portfolio";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
-import { AnimatePresence, createDomMotionComponent } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 
 const App = () => {
-  // const observer = new IntersectionObserver((entries) => {
-  //   entries.forEach((entry) => {
-  //     if (entry.isIntersecting) {
-  //       entry.target.classList.add("show");
-  //     } else {
-  //       entry.target.classList.remove("show");
-  //     }
-  //   });
-
-  // });
-
-  // const hiddenElements = document.querySelectorAll(".hidden")
-  // hiddenElements.forEach((e1) => observer.observe(e1))
-
-  const motion = {
-    div: createDomMotionComponent("div"),
-  };
-
   return (
     <>
       <AnimatePresence>
         <motion.div
-          initial={{ opacity: 0, y: 15 }}
-          animate={{ opacity: 0, y: 15 }}
-          exist={{ opacity: 0, y: 15 }}
-          transition={{ delay: 0.25 }}
+          
         >
           <Header />
           <Nav />
