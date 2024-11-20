28e3f8088a1fd69510bbddf9604cae65101ee1e1
Remidee <remi.aluko@expatswap.com>
2024-11-20 23:20:43 +0100

    updated

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,7 +4,11 @@ import RESUME from "../../Asset/resume.pdf";
 const CTA = () => {
   return (
     <div className="cta">
-      <a href="https://drive.google.com/file/d/1XNUKnGlRhBT-WpqKx4WzFrgrhU9_iqv9/view?usp=sharing" target="_blank" download className="btn">
+      <a
+        href="https://docs.google.com/document/d/1ueSJEq_VPaLan--mnQmC2aEH63DnF4wjFUTpo_Lan20/edit?usp=sharing"
+        target="_blank"
+        className="btn"
+      >
         Resume
       </a>
       <a href="#portfolio" className="btn btn-primary">
